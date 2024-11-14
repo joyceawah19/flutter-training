fa37f22dec33b6e57a7cccf80c3aa1dc5e4c0bc9
joyceawah19 <joyce.awah19@gmail.com>
2024-11-14 04:03:02 -0800

    first

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  print('Hello World');
+  printName();
+}
+
+void printName() {
+  print('joice fgfdfgsdfsdfsfds');
+}
+
+String name = 'Joice';
+int number = 1;
+double pi = 3.14;
+dynamic something = 3;
+List people = ['t', 'e'];
